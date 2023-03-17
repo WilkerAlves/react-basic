90d0d68d140deaa944a4fedea538b67fa5a06942
Wilker Alves <wilkeralves97@gmail.com>
2023-03-17 10:10:13 -0300

    jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  const name = "wilker"
+  const url = 'https://via.placeholder.com/150'
+
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
       <h1>Olá React</h1>
-      <p>Meu primeiro app</p>
+      <p>Olá {name}</p>
+      <p>Soma: {2 + 2}</p>
+      <p>SomaFunction: {sum(1, 1)}</p>
+      <img src={url} alt="minha imagem" />
     </div>
   );
 }
